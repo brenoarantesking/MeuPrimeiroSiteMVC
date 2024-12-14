397d5746f02c0ab56afeecc9c20786917b9673bd
brenoarantesking <breno_arantes@hotmail.com>
2024-12-14 08:08:26 -0300

    configuração do entity framework

---
+++ MeuPrimeiroSiteMVC/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿/*
+ * Configuração básica do Entity Framework
+ */
+
+using Microsoft.EntityFrameworkCore;
+using System.Runtime.Intrinsics.X86;
+
+namespace MeuPrimeiroSiteMVC.Data
+{
+    public class ApplicationDbContext : DbContext // cria uma classe derivada de DbContext, que serve como ponto de entrada para interagir com o banco de dados.
+    {
+
+    }
+}
