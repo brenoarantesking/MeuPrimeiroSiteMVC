bafe35646221c4ae5ea052e899d18ea1c15dd5fb
brenoarantesking <breno_arantes@hotmail.com>
2024-12-14 07:20:36 -0300

    criação da classe/model Category

---
+++ MeuPrimeiroSiteMVC/Models/Category.cs
@@ -0,0 +1,20 @@
+﻿/*
+ * Com o Entity Framework (ORM), não criamos tabela direto no banco de dados
+ * criamos um modelo/classe no projeto e o Entity Framework cria a tabela automaticamente.
+ */
+
+using System.ComponentModel.DataAnnotations;
+
+namespace MeuPrimeiroSiteMVC.Models
+{
+    public class Category // classe
+    {
+        // propriedades
+        [Key] // Data Annotattions - informa que o dado é chave primária
+        public int Id { get; set; } // se o nome for Id ou ClasseId, automaticamente será chave primária
+        [Required] // informa que o campo é requerido, não pode ser nulo - not null
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+
+    }
+}
