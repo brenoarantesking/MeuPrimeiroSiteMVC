d4bb665950a5979394fedb70cd01dd19627c42fa
brenoarantesking <breno_arantes@hotmail.com>
2024-12-15 12:27:44 -0300

    criado a tabela no banco de dados, usando add-migration e update-database

--- MeuPrimeiroSiteMVC/Data/ApplicationDbContext.cs
+++ MeuPrimeiroSiteMVC/Data/ApplicationDbContext.cs
@@ -2,6 +2,7 @@
  * Configuração básica do Entity Framework
  */
 
+using MeuPrimeiroSiteMVC.Models;
 using Microsoft.EntityFrameworkCore;
 using System.Runtime.Intrinsics.X86;
 
@@ -13,6 +14,8 @@ namespace MeuPrimeiroSiteMVC.Data
         {
             
         }
+
+        public DbSet<Category> Categories { get; set; } // DbSet<classe> nome_tabela - cria uma tabelas no banco de dados com o nome Categories
 
 
     }
